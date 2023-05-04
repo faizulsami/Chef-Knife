f5de06799be007f802e24b44187ca8a890e4f727
faizulsami <faizul.sami2021@gmail.com>
2023-05-04 23:55:37 +0600

    FireBase Protection Added

--- src/FireBase/firebase.config.js
+++ src/FireBase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCdIsVaeFeIsx0nZRuNfzycg8w6ghWDYJo",
-  authDomain: "assignment-10-a8774.firebaseapp.com",
-  projectId: "assignment-10-a8774",
-  storageBucket: "assignment-10-a8774.appspot.com",
-  messagingSenderId: "947440069191",
-  appId: "1:947440069191:web:d28e1da2de03290c9c1f62"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
